c65d70a97427d55a2b1acfa26185b79d26c65252
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2023-04-11 11:22:04 +0200

    eerste versie

---
+++ Arduino_Programmeren_Template.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  // Hier ga je de verschillende inputs en outputs configureren
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
